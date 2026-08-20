ae8d0ec2f69da84719aa3124223389caf1420972
uruzinfo1-art <uruzinfo1@gmail.com>
2026-08-20 15:09:31 -0500

    Corregir autenticacion Vonage

--- api/whatsapp/inbound.ts
+++ api/whatsapp/inbound.ts
@@ -3,8 +3,7 @@ import { Vonage } from "@vonage/server-sdk";
 
 const vonage = new Vonage({
   apiKey: process.env.VONAGE_API_KEY!,
-  applicationId: process.env.VONAGE_APPLICATION_ID!,
-  privateKey: process.env.VONAGE_PRIVATE_KEY!,
+  apiSecret: process.env.VONAGE_API_SECRET!,
 });
 
 export default async function handler(
